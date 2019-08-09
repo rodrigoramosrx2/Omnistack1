5fe3710735184f213cd15b7bb8c12ed214b8f8fe
Rodrigo Ramos <rodrigoramosrx2@gmail.com>
2019-08-08 23:48:58 -0300

    Aula2

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack123456@cluster0-offf6.mongodb.net/omnistack1?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
